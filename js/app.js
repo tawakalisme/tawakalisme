423b643ab0a8b59f8c663b0e3076120eff391c9c
tawakalisme <mcbalbal@Live.com>
2019-02-23 11:55:57 +0700

    added social media button. done for first page.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 particlesJS.load("particle-bg", "js/particles-config.json");
+var parent = document.getElementsByTagName('body');
+var child = document.getElementById('first-section');
+
+child.style.paddingRight = child.offsetWidth - child.clientWidth + "px";
